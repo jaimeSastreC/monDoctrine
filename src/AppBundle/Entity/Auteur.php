cecea8409a2b4d97f13a1a3f7b3884d5fcab1bbf
lapiscine <jaime.sastre@lapiscine.pro>
2018-11-21 16:51:42 +0100

    1ere sauvegarde

--- src/AppBundle/Entity/Auteur.php
+++ src/AppBundle/Entity/Auteur.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+/*classe Mapping*/
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Auteur
 {
+
     //Id est la clé primaire !!! Annotation qui aide à gérer tables Doctrine
     /**
      * @ORM\Column(type="integer")
@@ -18,30 +20,97 @@ class Auteur
      */
     private $id;
 
+    // varcharacter
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=100, name="nom")
      */
-    private $auteur;
+    private $nom;
 
     /**
      * @ORM\Column(type="datetime", name="date_naissance")
      */
-    private $date_naissance;
+    private $dateNaissance;
 
     /**
      * @ORM\Column(type="datetime", name="date_mort", nullable=true)
      */
-    private $date_mort;
-
-     /**
-      * @ORM\Column(type="integer")
-      */
-    private $pages;
+    private $dateMort;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="text", name="biographie")
      */
     private $biographie;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * @param mixed $dateNaissance
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateMort()
+    {
+        return $this->dateMort;
+    }
+
+    /**
+     * @param mixed $dateMort
+     */
+    public function setDateMort($dateMort)
+    {
+        $this->dateMort = $dateMort;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBiographie()
+    {
+        return $this->biographie;
+    }
+
+    /**
+     * @param mixed $biographie
+     */
+    public function setBiographie($biographie)
+    {
+        $this->biographie = $biographie;
+    }
 
 }
